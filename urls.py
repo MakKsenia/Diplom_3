f194321cc0b4a06d1ee48e742473903d7c8a014e
MakKsenia <ksenia12209@mail.ru>
2025-05-26 21:49:16 +0300

    new commit

---
+++ urls.py
@@ -0,0 +1,6 @@
+MAIN_URL = 'https://stellarburgers.nomoreparties.site/'
+LOGIN_PAGE = 'https://stellarburgers.nomoreparties.site/login'
+FORGOT_PASSWORD_PAGE = 'https://stellarburgers.nomoreparties.site/forgot-password'
+RESET_PASSWORD_PAGE = 'https://stellarburgers.nomoreparties.site/reset-password'
+ORDER_HISTORY = 'https://stellarburgers.nomoreparties.site/account/order-history'
+ORDER_FEED = 'https://stellarburgers.nomoreparties.site/feed'
